539a5e6182b09dee94abb3cd46a545d1f01893b1
tuychiyevkm <tmkomron@gmail.com>
2025-08-08 18:26:21 +0500

    universal echo bot has finished

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Contact, Update
 from telegram.ext import CallbackContext
 
 
@@ -15,3 +15,66 @@ def handle_help(update:Update, context: CallbackContext):
 
 def handle_echo(update:Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
+
+
+def echo_photo(update:Update, context: CallbackContext):
+    photo = update.message.photo[0]
+    update.message.reply_photo(photo=photo)
+
+
+def echo_contact(update:Update, context: CallbackContext):
+    contact = update.message.contact
+    update.message.reply_contact(contact=contact)
+   
+def echo_video(update:Update, context:CallbackContext):
+    video = update.message.video
+    update.message.reply_video(video=video)
+
+
+def echo_sticker(update:Update, context: CallbackContext):
+    sticker = update.message.sticker
+    update.message.reply_sticker(sticker=sticker)
+
+
+def echo_video_note(update: Update, context: CallbackContext):
+    video_note = update.message.video_note
+    update.message.reply_video_note(video_note=video_note)
+
+def echo_audio(update: Update, context: CallbackContext):
+    audio = update.message.audio
+    update.message.reply_audio(audio=audio)
+
+def echo_animation(update: Update, context: CallbackContext):
+    animation = update.message.animation
+    update.message.reply_animation(animation=animation)
+
+
+def echo_location(update: Update, context: CallbackContext):
+    location = update.message.location
+    update.message.reply_location(
+        latitude=location.latitude,
+        longitude=location.longitude
+    )
+
+
+def echo_dice(update: Update, context: CallbackContext):
+    dice = update.message.dice
+    update.message.reply_dice(emoji=dice.emoji)
+
+
+def echo_document(update: Update, context: CallbackContext):
+    document = update.message.document
+    update.message.reply_document(document=document)
+
+
+def echo_voice(update: Update, context: CallbackContext):
+    voice = update.message.voice
+    update.message.reply_voice(voice=voice)
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from handlers import handle_start, handle_help, handle_echo
+from handlers import ( 
+                        handle_start,      handle_help,     handle_echo,    echo_photo, 
+                        echo_contact,      echo_video,      echo_sticker,   echo_video_note, 
+                        echo_animation,    echo_location,   echo_dice, echo_document, 
+                        echo_voice,        echo_audio
+                        )
 from config import TOKEN
 
 def main() -> None:
@@ -12,6 +17,18 @@ def main() -> None:
 
     # message handler
     dispatcher.add_handler(MessageHandler(Filters.text, handle_echo))
+    dispatcher.add_handler(MessageHandler(Filters.contact, echo_contact))
+    dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo))
+    dispatcher.add_handler(MessageHandler(Filters.sticker, echo_sticker))
+    dispatcher.add_handler(MessageHandler(Filters.dice, echo_dice))
+    dispatcher.add_handler(MessageHandler(Filters.video, echo_video))
+    dispatcher.add_handler(MessageHandler(Filters.audio, echo_audio))
+    dispatcher.add_handler(MessageHandler(Filters.voice, echo_voice))
+    dispatcher.add_handler(MessageHandler(Filters.animation, echo_animation))
+    dispatcher.add_handler(MessageHandler(Filters.location, echo_location))
+    dispatcher.add_handler(MessageHandler(Filters.video_note, echo_video_note))
+    dispatcher.add_handler(MessageHandler(Filters.document, echo_document))
+
 
     updater.start_polling()
     updater.idle()
